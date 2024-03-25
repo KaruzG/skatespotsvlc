d8a053378ca2c2e9e81c42c08783e046b668224a
KaruzG <josemiguelcpcs@gmail.com>
2024-03-25 21:29:24 +0100

    spotBubble base

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import Header from '../components/Header'
+import SpotBubble from '../components/SpotBubble'
 import '../css/index.scss'
+
+
 
 function Home() {
     return (
         <>
             <Header />
+            <SpotBubble spotName='muvim' srcImg='muvim' />
         </>
     )
 }
