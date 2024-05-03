ea5deaab38ee64213a3d0a4227de5c8d42dcea94
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-05-03 19:44:19 +0200

    fix config

---
+++ server/dist/routes/users.js
@@ -0,0 +1,37 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const express_1 = __importDefault(require("express"));
+const User_1 = __importDefault(require("../database/models/User"));
+const router = express_1.default.Router();
+router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield mongoose_1.default.connect(process.env.MONGO_DB_URL);
+    const { body } = req;
+    const { username, password, type } = body;
+    const user = new User_1.default({
+        type,
+        username,
+        password: password,
+        creation: new Date()
+    });
+    const savedUser = yield user.save();
+    res.json(savedUser);
+    mongoose_1.default.connection.close();
+}));
+router.get('/', (req, res) => {
+    res.send("USER!");
+});
+exports.default = router;
+//# sourceMappingURL=users.js.map
