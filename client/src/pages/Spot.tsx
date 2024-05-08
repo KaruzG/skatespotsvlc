51ccd026958b5108d3481cd6243a8e8e7ba6773f
Miguel Gresa <95084763+KaruzG@users.noreply.github.com>
2024-05-08 14:27:26 +0000

    spotPage base (#10)

--- client/src/pages/Spot.tsx
+++ client/src/pages/Spot.tsx
@@ -1,10 +1,16 @@
+import BestSpotsSection from '../components/BestSpotsSection'
 import Header from '../components/Header'
+import SpotPageMain from '../components/SpotPageMain'
+import SpotsMap from '../components/SpotsMap'
+import "../css/index.scss"
 
 function Spot() {
     return (
         <>
             <Header />
-            
+            <SpotPageMain/>
+            <SpotsMap />
+            <BestSpotsSection />
         </>
     )
 }
