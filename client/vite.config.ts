92f23e34260c0e42347691d85eb97bd47ecd40ca
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-05-08 20:56:36 +0200

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'pages/admin.html'),
+        spot: resolve(__dirname, 'pages/spot.html'),
+        about: resolve(__dirname, 'pages/about.html'),
+      },
+    },
+  },
 })
