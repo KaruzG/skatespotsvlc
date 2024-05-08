186d2e9481143f13dba3430044b16ebcbaa06051
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-05-08 15:59:21 +0200

    form styles

--- client/src/components/AddSpotForm/AddSpotForm.tsx
+++ client/src/components/AddSpotForm/AddSpotForm.tsx
@@ -9,12 +9,15 @@ const AddSpotForm = () => {
         <h3>New Spot</h3>
 
         <form action="">
+          <label htmlFor="">Basic info</label>
           <input type="text" name="" placeholder="Spot Name" id="" />
-          <input type="text" name="" placeholder="Description" id="" />
+          <textarea name="" id="" placeholder='Description'></textarea>
+          <label htmlFor="">Coords</label>
           <div className='coords'>
             <input type="text" name="" placeholder="Altitude" id="" />
             <input type="text" name="" placeholder="Latitude" id="" />
           </div>
+          <label htmlFor="">Image of the spot</label>
           <input type="file" name="" placeholder="Spot Name" id="" />
           <Button color='orange' innerText='Submit' style='fill' size='s'/>
         </form>
