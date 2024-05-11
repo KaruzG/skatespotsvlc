3130ed8595611b543f65c1c64f3c79964c716c0c
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-05-11 14:33:17 +0200

    Footer component

--- client/src/pages/Error404.tsx
+++ client/src/pages/Error404.tsx
@@ -1,4 +1,5 @@
 import Button from '../components/Button'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../css/index.scss'
 
@@ -13,6 +14,7 @@ function Error404() {
             <Button color='orange' style='empty' size='l' link={"/"}>HOME</Button>
             </div>
 
+            <Footer/>
         </div>
     )
 }
