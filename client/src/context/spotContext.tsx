0fcfcba5c62a6a5ecb46b1b845a554be3ba8d645
Miguel Gresa <95084763+KaruzG@users.noreply.github.com>
2024-05-20 19:49:17 +0200

    Update spotContext.tsx

--- client/src/context/spotContext.tsx
+++ client/src/context/spotContext.tsx
@@ -2,5 +2,5 @@ import { createContext } from "react";
 import { SpotData } from "../types/spotData";
 
 
-
+// @ts-expect-error This is working fine like this but should fix te typing
 export const SpotContext = createContext<SpotData>({})
