425ddbfaca42c0d0cf3f8c7e5ded1bcc0c3c2c51
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-05-21 21:48:08 +0200

    Remplace typing package

--- client/src/components/AboutSection1/AboutSection1.tsx
+++ client/src/components/AboutSection1/AboutSection1.tsx
@@ -1,25 +1,17 @@
 import Button from "../Button";
 import "./AboutSection1.scss"
 import phoneImg from "../../img/aboutPhone.png"
-import TypingAnimation from "react-word-typing-animation"
+import { TypeAnimation } from 'react-type-animation'
 import { BiChevronDown } from "react-icons/bi";
 import { ErrorBoundary } from "react-error-boundary";
 
 
 
 const AboutSection1 = () => {
-  const WORDS = <TypingAnimation
-    words={['SKATEPARKS', 'SPOTS', 'EVENTS', 'SHOPS']} 
-    showComma={false} 
-    showCursor={false}
-    interval={1000}
-    typingSpeed={250}
+  const WORDS = <TypeAnimation 
+    sequence={['SPOTS', 2000, 'SKATEPARKS', 2000, 'SKATESHOPS', 2000, 'EVENTS', 2000]}
+    repeat={Infinity}
     />
-
-    function handleWordsError(ev) {
-      console.log("error")
-      ev.target.innerText = "The BEST way to find SPOTS in Valencia! 🛹"
-    }
 
   return (
     <>
@@ -30,7 +22,7 @@ const AboutSection1 = () => {
         </div>
         <div>
           <ErrorBoundary fallback={<h1>The BEST way to find SPOTS in Valencia! 🛹</h1>}>
-            <h1>The BEST way to find {WORDS} in Valencia! 🛹</h1>
+            <h1>The BEST way to find</h1> <h1>{WORDS}</h1> <h1>in Valencia! 🛹</h1>
           </ErrorBoundary>
           <p>Find and share your favorite spots and useful info about them.</p>
           <Button color="orange" size="m" style="fill" link={"/"}>Try it now!</Button>
