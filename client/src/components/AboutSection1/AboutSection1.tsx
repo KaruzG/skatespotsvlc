0a816e1c79d8cccadfada0d4e2f77be71cda3650
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-05-21 20:48:21 +0200

    ErrorBoundary

--- client/src/components/AboutSection1/AboutSection1.tsx
+++ client/src/components/AboutSection1/AboutSection1.tsx
@@ -3,17 +3,23 @@ import "./AboutSection1.scss"
 import phoneImg from "../../img/aboutPhone.png"
 import TypingAnimation from "react-word-typing-animation"
 import { BiChevronDown } from "react-icons/bi";
+import { ErrorBoundary } from "react-error-boundary";
 
 
 
 const AboutSection1 = () => {
-  const WORDS = <TypingAnimation 
+  const WORDS = <TypingAnimation
     words={['SKATEPARKS', 'SPOTS', 'EVENTS', 'SHOPS']} 
     showComma={false} 
     showCursor={false}
     interval={1000}
     typingSpeed={250}
     />
+
+    function handleWordsError(ev) {
+      console.log("error")
+      ev.target.innerText = "The BEST way to find SPOTS in Valencia! 🛹"
+    }
 
   return (
     <>
@@ -23,7 +29,9 @@ const AboutSection1 = () => {
           <div id="blurredCircle"></div>
         </div>
         <div>
-          <h1>The BEST way to find {WORDS} in Valencia! 🛹</h1>
+          <ErrorBoundary fallback={<h1>The BEST way to find SPOTS in Valencia! 🛹</h1>}>
+            <h1>The BEST way to find {WORDS} in Valencia! 🛹</h1>
+          </ErrorBoundary>
           <p>Find and share your favorite spots and useful info about them.</p>
           <Button color="orange" size="m" style="fill" link={"/"}>Try it now!</Button>
         </div>
